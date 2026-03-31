98a34d5eda42420f50868d35f4de729c2ce53e03
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-31 10:19:32 +0800

    memilih menu

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -10,3 +10,8 @@ int menu() {
     cout << "1. Cek Bilangan Prima" << endl;
     cout << "2. Cek Bilangan Fibonacci" << endl;
     cout << "0. Keluar" << endl;
+    cout << "Pilih menu: ";
+    cin >> pilihan;
+    return pilihan;
+}
+
